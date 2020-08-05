cb79bbd75f9d361bcbb1a32254cfb4d2e460d142
MikhailRommulo <mikhailrommulo@gmail.com>
2020-08-05 17:33:31 -0300

    aula 3

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -3,32 +3,52 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
-function TeacherItem() {
+export interface Teacher {
+  id: 2;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({teacher}) => {
+    function createNewConnection() {
+      api.post('connections', {
+        user_id: teacher.id
+      });
+    }
+
     return (
         <article className="teacher-item">
         <header>
-          <img src="https://avatars3.githubusercontent.com/u/37477960?s=460&u=b43ea639202d3a838a01793bba2ccf61d8256b6c&v=4" alt="Mikhail Jesus"/>
+          <img src={teacher.avatar} alt={teacher.name}/>
           <div>
-            <strong>Mikhail Rommulo</strong>
-            <span>Química</span>
+            <strong>{teacher.name}</strong>
+            <span>{teacher.subject}</span>
           </div>
         </header>
-        <p>
-          Entusiasta das melhores tecnologias de química avançada.
-          <br /><br />
-          Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências
-        </p>
+        <p>{teacher.bio}</p>
 
         <footer>
             <p>
               Preço/hora 
-              <strong>R$ 80,00</strong>
+              <strong>R$ {teacher.cost}</strong>
             </p>
-            <button type="button">
+            <a 
+              target="_blank"
+              onClick={createNewConnection}
+              href={`https://wa.me/${teacher.whatsapp}`}
+            >
               <img src={whatsappIcon} alt="Entrar em contato"/>
               Entrar em contato
-            </button>
+            </a>
         </footer>
       </article>
     );
